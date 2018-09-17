823b27181b8988bf428c74c65f24a33e64ede636
mattia <mattia.bacci@stud.unifi.it>
2018-09-17 15:58:32 +0200

    Created Matrix class

---
+++ Matrix.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mattia Bacci on 17/09/18.
+//
+
+#include "Matrix.h"
---
+++ Matrix.h
@@ -0,0 +1,14 @@
+//
+// Created by Mattia Bacci on 17/09/18.
+//
+
+#ifndef MATRIX_MATRIX_H
+#define MATRIX_MATRIX_H
+
+
+class Matrix {
+
+};
+
+
+#endif //MATRIX_MATRIX_H
